c3b2a49211c67128439e7b3c349c69c3d6bb0656
AshleeKaye <ashleemk@hotmail.com>
2018-06-08 17:16:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # dog.rb
+class Dog
+  def dog=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  def dog
+    @this_dogs_name
+  end
+  def dog2=(dog2_name)
+    @this_dogs_name2 = dog2_name
+  end
+  def dog2
+    @this_dogs_name2
+  end
+  def dog3=(dog3_name)
+  @this_dogs_name3 = dog3_name
+end
+def dog3
+  @this_dogs_name3
+end
+
+
+
+dog_name = "fido"
+dog2_name = "snoopy"
+dog3_name = "lassie"
+puts "The dogs names are #{dog_name}, #{dog2_name}, and #{dog3_name}."
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+class Person
+  def name=(name1)
+    @this_person_name = (name1)
+  end
+  def name
+    @this_person_name
+  end
+  def name2=(name2)
+    @this_person_name2 = (name2)
+  end
+  def name2
+    @this_person_name2
+  end
+  name1 = "adel_goldberg"
+  name2 = "alan_kay"
+  puts "These peoples names are #{name1} and #{name2}."
+end
